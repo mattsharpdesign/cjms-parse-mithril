a8f2545f48e124396a4e2d60e95f1f30d383ae21
matt <matt@mattsharp.co.nz>
2022-04-24 01:10:11 +1200

    add example of afterFind to cloud code

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,8 @@
 // Parse.Cloud.beforeFind('Customer', req => {
   // console.log(req.query)
 // })
+
+// Parse.Cloud.afterFind('Job', req => {
+//   const first = req.objects[0]
+//   console.log(first.get('orderDate'))
+// })
